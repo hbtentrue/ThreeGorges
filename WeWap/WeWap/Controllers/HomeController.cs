6514cf6dacaa17b595a5756d145bf061172e4d33
longanD <783213408@qq.com>
2017-10-18 11:50:14 +0800

    准备提交到远程git

---
+++ WeWap/WeWap/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeCan.BusinessLogic;
+using WeCan.Models;
+
+namespace WeWap.Controllers
+{
+    /// <summary>
+    /// 常规移动端资讯类项目
+    /// </summary>
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 默认页
+        /// </summary>
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 清单
+        /// </summary>
+        public ActionResult List(Pagination pn)
+        {
+            var biz = new ItemBiz();
+            var list = biz.GetItemList(pn);
+            return View(list);
+        }
+        /// <summary>
+        /// 详情
+        /// </summary>
+        public ActionResult Details(ItemSummaryM item)
+        {
+            var biz = new ItemBiz();
+            item = biz.GetItemById(1,1,1,"");
+            return View();
+        }
+        /// <summary>
+        /// 分部视图
+        /// </summary>
+        /// <param name="id">静态视图的名称</param>
+        public ActionResult PartView(string id)
+        {
+            return PartialView(id);
+        }
+    }
+}
---
+++ WeWap/WeWap/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WeWap
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            string filepath = Server.MapPath("~/");
+            AppSets.InitialAppSetting(this.GetType().BaseType.Namespace);
+            Configer.InitialConfig(filepath, "WeCanConfig");
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
